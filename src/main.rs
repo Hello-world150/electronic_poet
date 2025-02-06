c331f2f22469cff749502ec1fa7c025c0ca421af
Haolin Qin <qhl314159265@126.com>
2025-02-07 03:18:00 +0800

    feat:add line and paragraph argument choose support

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use exitcode::IOERR;
+use exitcode::{DATAERR, IOERR};
 use rand::{rng, Rng};
 use std::{fs::read_to_string, process::exit};
+use structopt::StructOpt;
 
+/// 电子诗人
+#[derive(StructOpt)]
+struct Args {
+    /// 每段行数
+    line: u128,
+    /// 总段数
+    paragraph: u128,
+}
 fn file_to_vec(path: &str) -> Vec<String> {
     match read_to_string(path) {
         Ok(s) => {
             s.split("\n").map(|s| s.to_string()).collect() // return `Vec<String>` type
         }
-        Err(_) => {
-            eprintln!("打开 {path} 失败");
+        Err(error) => {
+            eprintln!("{error}: {path}");
             exit(IOERR);
         }
     }
@@ -20,6 +29,14 @@ fn get_rand_element(vec: &[String]) -> String {
 }
 
 fn main() {
+    let args = match Args::from_args_safe() {
+        Ok(args) => args,
+        Err(error) => {
+            eprintln!("{error}");
+            exit(DATAERR);
+        }
+    };
+
     let v = file_to_vec("v.txt");
     let t = file_to_vec("t.txt");
     let i = file_to_vec("i.txt");
@@ -27,20 +44,25 @@ fn main() {
     let n = file_to_vec("n.txt");
     let sentence = file_to_vec("sentence.txt");
 
-    for _times in 1..50 {
-        let sentence = get_rand_element(&sentence);
+    for _para_number in 1..=args.paragraph {
+        for _line_number in 1..=args.line {
+            let sentence = get_rand_element(&sentence);
 
-        let mut result = String::new();
-        for current_char in sentence.chars() {
-            match current_char {
-                'v' => result.push_str(&get_rand_element(&v)),
-                't' => result.push_str(&get_rand_element(&t)),
-                'i' => result.push_str(&get_rand_element(&i)),
-                'a' => result.push_str(&get_rand_element(&a)),
-                'n' => result.push_str(&get_rand_element(&n)),
-                _ => result.push_str(&current_char.to_string()[..]), // as `&str` type
+            let mut line_result = String::new();
+            for current_char in sentence.chars() {
+                match current_char {
+                    'v' => line_result.push_str(&get_rand_element(&v)),
+                    't' => line_result.push_str(&get_rand_element(&t)),
+                    'i' => line_result.push_str(&get_rand_element(&i)),
+                    'a' => line_result.push_str(&get_rand_element(&a)),
+                    'n' => line_result.push_str(&get_rand_element(&n)),
+                    _ => line_result.push_str(&current_char.to_string()[..]), // as `&str` type
+                }
             }
+
+            println!("{line_result}");
         }
-        println!("{result}");
+
+        println!();
     }
 }
